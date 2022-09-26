936f9f8eaf2355d8526769337e01ae3d274a6bda
jaishmino5 <jaishminocha@jklu.edu.in>
2022-09-26 19:21:39 +0530

    updated on 26/09/22

--- ajjjjajajajaj.java
+++
@@ -1,5 +0,0 @@
-package jaish;
-
-public class ajjjjajajajaj {
-
-}
---
+++ hello.java
@@ -0,0 +1,7 @@
+package jaish;
+
+class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
