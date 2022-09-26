f01526399e8b2bd35b36ddd429fa9d378a417bff
jaishmino5 <jaishminocha@jklu.edu.in>
2022-09-26 19:06:22 +0530

    jaish

---
+++ ajjjjajajajaj.java
@@ -0,0 +1,5 @@
+package jaish;
+
+public class ajjjjajajajaj {
+
+}
---
+++ jjjjjjjjj.java
@@ -0,0 +1,17 @@
+package jaish;
+import java.util.Scanner;
+public class jjjjjjjjj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++) {
+			for(int k=0;k<a;k++) {
+				System.out.println("*");
+			}
+		}
+
+	}
+
+}
